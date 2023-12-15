b9b7d13f5c932902fdf175bb9c018bc57d0ad392
JakeJeongAiden <146908415+JakeJeongAiden@users.noreply.github.com>
2023-12-14 18:54:31 -0800

    Prevent from going 0

--- @4.4.1/src/CountButton.jsx
+++ @4.4.1/src/CountButton.jsx
@@ -5,7 +5,11 @@ export default function CountButton ({ type, setNumber }) {
     //count + 1 but count is not defined, so we use prev. This is second way of using setNumber.
     setNumber(prev => {
       if (type === 'minus') {
-        return prev - 1
+        const newCount = prev - 1
+        if (newCount < 0) {
+          return 0
+        }
+        return newCount
       } else {
         return prev + 1
       }
